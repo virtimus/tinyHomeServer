3a125d4b468159d5e5494b18ae77a0aac6b6ebc3
virtimus <virtimus@gmail.com>
2020-12-30 15:46:10 +0100

    sfix

--- features-dev/makaronLab/verilog-course/lecture3/testbench.v
+++ features-dev/makaronLab/verilog-course/lecture3/testbench.v
@@ -3,7 +3,7 @@ module testbench;
 	example DUT(A,B,C,D,E,F,Y);
 	
 	initial begin
-		$monitor ($time, "A=%b, B=%b, C=%b, D=%b, E=%b, F=%b, Y=%b", A,B,C,D,E,F,Y);
+		$monitor ($time, " A=%b, B=%b, C=%b, D=%b, E=%b, F=%b, Y=%b", A,B,C,D,E,F,Y);
 		#5 A=1; B=0; C=0; D=1; E=0; F=0;
 		#5 A=0; B=0; C=1; D=1; E=0; F=0;
 		#5 A=1; C=0; 
